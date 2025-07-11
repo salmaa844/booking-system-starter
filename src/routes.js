117d6cb1faab59aa4adbdc6e6852a3aea6ced501
salma ali <alisalma455@gmail.com>
2025-07-11 17:15:31 +0300

    create routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
-// import globalErrorHandler from "./middlewares/globalErrorHandler.js";
-// import authRouter from "./modules/auth/auth.router.js";
-// import userRouter from "./modules/user/user.router.js";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import authRouter from "./modules/auth/auth.router.js";
+import userRouter from "./modules/user/user.router.js";
 // import providerRouter from "./modules/provider/provider.router.js";
-// import bookingRouter from "./modules/booking/booking.router.js";
+import bookingRouter from "./modules/booking/booking.router.js";
 
 export function init(express, app) {
   app.use(express.json());
 
   // TODO: Uncomment and implement the routes below to activate the modules
 
-  // app.use("/auth", authRouter);          // Auth routes for login/register/confirm email
-  // app.use("/users", userRouter);          // User management routes
-  // app.use("/bookings", bookingRouter);    // Booking system routes
+  app.use("/auth", authRouter);          // Auth routes for login/register/confirm email
+  app.use("/users", userRouter);          // User management routes
+  app.use("/bookings", bookingRouter);    // Booking system routes
 
   // TODO: Uncomment the global error handler middleware after implementing it
-  // app.use(globalErrorHandler);
+  app.use(globalErrorHandler);
 }
