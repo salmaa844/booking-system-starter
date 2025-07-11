ce68109e9d1e23c1fed9751566ac99c75389030a
salma ali <alisalma455@gmail.com>
2025-07-11 17:19:54 +0300

    remove the space before the foreign key name

--- Database/associations.js
+++ Database/associations.js
@@ -1,5 +1,5 @@
 import User from "./models/user.model.js"
 import Booking from "./models/booking.model.js"
 
-User.hasMany(Booking,{foreignKey:" userId"});
-Booking.belongsTo(User,{foreignKey:" userId"});
+User.hasMany(Booking,{foreignKey:"userId"});
+Booking.belongsTo(User,{foreignKey:"userId"});
