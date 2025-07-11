bb14426cdbfc8a1e9d17ab7a611251417ffe3b49
salma ali <alisalma455@gmail.com>
2025-07-11 17:17:09 +0300

    add logging in seqelize

--- Database/config.js
+++ Database/config.js
@@ -3,7 +3,8 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host:process.env.DB_HOST,
-  dialect:process.env.DB_DIALECT
+  dialect:process.env.DB_DIALECT,
+  logging:false
 });
 
 export default sequelize;
